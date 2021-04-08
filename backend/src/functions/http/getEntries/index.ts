5be6205eb914ed0c617bb834494b751bb0294603
kotsonis <stefankotsonis@gmail.com>
2021-04-08 19:48:08 +0200

    fix: update AWS resource name

--- backend/src/functions/http/getEntries/index.ts
+++ backend/src/functions/http/getEntries/index.ts
@@ -23,7 +23,7 @@ export default {
         Action: [
           'dynamodb:Query',
         ],
-        Resource: ["arn:aws:dynamodb:${self:provider.region}:*:table/${self:provider.environment.TODOS_TABLE}"]
+        Resource: ["arn:aws:dynamodb:${self:provider.region}:*:table/${self:provider.environment.ENTRIES_TABLE}"]
     }
   ]
 }
