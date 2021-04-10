a18726c9bc46bd546df0c0a6b68c07a8cc725020
kotsonis <stefankotsonis@gmail.com>
2021-04-10 17:47:53 +0200

    refactor: easier to read UpdateExpression

--- backend/src/libs/database.ts
+++ backend/src/libs/database.ts
@@ -110,20 +110,20 @@ export async function updateItemStatus(sortKey: string, user: string, newStatus:
   return result
 }
 export async function updateItem(sortKey: string, user: string, newEntry: JournalEntry) {
-  var dbUpdateExpression = "SET #ts = :entryDate"
+  let dbUpdateExpression = "SET #ts = :entryDate"
   const dbExpressionAttributeValues = {
     ":entryDate": newEntry.entryDate
   }
   if(newEntry.hasOwnProperty('description')){
-    dbUpdateExpression = dbUpdateExpression.concat(', description = :desc')
+    dbUpdateExpression += ', description = :desc'
     dbExpressionAttributeValues[":desc"] = newEntry.description
   }
   if(newEntry.hasOwnProperty('headline')) {
-    dbUpdateExpression = dbUpdateExpression.concat(', name = :headline')
+    dbUpdateExpression += ', name = :headline'
     dbExpressionAttributeValues[":headline"] = newEntry.headline
   }
   if(newEntry.hasOwnProperty('mood')) {
-    dbUpdateExpression = dbUpdateExpression.concat(', mood = :m')
+    dbUpdateExpression += ', mood = :m'
     dbExpressionAttributeValues[":m"] = newEntry.mood
   }
   
